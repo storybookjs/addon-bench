69c6abf90474619de67d47d68f3469b81e58100b
Norbert de Langen <ndelangen@me.com>
2022-08-30 13:50:02 +0200

    change host to localhost

--- src/sbbench.js
+++ src/sbbench.js
@@ -1,4 +1,4 @@
-export const HOST = '127.0.0.1';
+export const HOST = 'localhost';
 export const PORT = 9898;
 export const MANAGER_RENDER = 'managerRender';
 export const PREVIEW_RENDER = 'previewRender';
