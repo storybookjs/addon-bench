22c2481f0da7c061d3bba2034d2d91a74eed46e8
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-10-20 21:58:15 -0400

    Wait until First Contentful Paint

--- src/sbbench.js
+++ src/sbbench.js
@@ -5,6 +5,45 @@ export const PREVIEW_RENDER = 'previewRender';
 export const STORY_RENDER = 'storyRender';
 
 export const logEvent = event => {
-  console.log('Logging', event);
-  fetch(`http://${HOST}:${PORT}/${event}`);
+  function sendPing() {
+    console.log('Logging', event);
+    fetch(
+      'http://'
+        .concat(HOST, ':')
+        .concat(PORT, '/')
+        .concat(event)
+    );
+  }
+
+  if ('performance' in window) {
+    const paintMetrics = performance.getEntriesByType('paint');
+    const FCPs =
+      paintMetrics !== undefined
+        ? paintMetrics.filter(m => m.name === 'first-contentful-paint')
+        : [];
+    // Check if first-contentful-paint has already happened
+    if (FCPs.length > 0) {
+      console.log(
+        'first-contentful-paint already happened, time may be inflated'
+      );
+      sendPing();
+    } else {
+      const observer = new PerformanceObserver(function(list, obs) {
+        var entries = list
+          .getEntries()
+          .filter(e => e.name === 'first-contentful-paint');
+        for (const entry of entries) {
+          if (entry.name === 'first-contentful-paint') {
+            sendPing();
+            obs.disconnect();
+          }
+        }
+      });
+      observer.observe({ type: 'paint' });
+    }
+  } else {
+    // Not able to use paint timing, just send the fetch now.
+    console.log('skipping first-contentful-paint');
+    sendPing();
+  }
 };
